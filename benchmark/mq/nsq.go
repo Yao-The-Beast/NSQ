d23afda12ef3a263b7bc46b234e361de77b1a770
Yao-The-Beast <drtailor@drtailors-mbp.dhcp.wustl.edu>
2016-04-04 12:50:57 -0500

    Modified NSQ Benchmark

---
+++ benchmark/mq/nsq.go
@@ -0,0 +1,73 @@
+package mq
+
+import (
+	"strconv"
+	"github.com/bitly/go-nsq"
+	"github.com/tylertreat/mq-benchmarking/benchmark"
+)
+
+type Nsq struct {
+	handler benchmark.MessageHandler
+	pub     *nsq.Producer
+	sub     *nsq.Consumer
+	topic   string
+	channel string
+        raw_channel string
+}
+
+func NewNsq(numberOfMessages int, testLatency bool, channeL string) *Nsq {
+	//topic := "0#ephemeral"
+	//topic := "0"
+	channel := channeL
+	channel += "#ephemeral"
+	topic := channel	
+	pub, _ := nsq.NewProducer("localhost:4150", nsq.NewConfig())
+	config :=nsq.NewConfig()
+	config.MaxInFlight = 2000
+	sub, _ := nsq.NewConsumer(topic, channel, config)
+	var handler benchmark.MessageHandler
+	if testLatency {
+		handler = &benchmark.LatencyMessageHandler{
+			NumberOfMessages: numberOfMessages,
+			Latencies:        []float32{},
+			Channel:	channeL,
+		}
+	} else {
+		handler = &benchmark.ThroughputMessageHandler{NumberOfMessages: numberOfMessages}
+	}
+
+	return &Nsq{
+		handler: handler,
+		pub:     pub,
+		sub:     sub,
+		topic:   topic,
+		channel: channel,
+		raw_channel: channeL,
+	}
+}
+
+func (n *Nsq) Setup() {
+	n.sub.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		n.handler.ReceiveMessage(message.Body)
+		return nil
+	}))
+	i, _ := strconv.Atoi(n.raw_channel)
+	if i < 128 {
+		n.sub.ConnectToNSQD("192.168.1.12:4150")
+	} else {
+		n.sub.ConnectToNSQD("localhost:4150")
+	}
+}
+
+func (n *Nsq) Teardown() {
+	n.sub.Stop()
+	n.pub.Stop()
+}
+
+func (n *Nsq) Send(message []byte) {
+	//n.pub.PublishAsync(n.topic, message, nil)
+}
+
+func (n *Nsq) MessageHandler() *benchmark.MessageHandler {
+	return &n.handler
+}
---
+++ benchmark/receiver.go
@@ -0,0 +1,153 @@
+package benchmark
+
+import (
+	"encoding/binary"
+	"log"
+	"sync"
+	"time"
+	"io/ioutil"
+	"strconv"	
+)
+
+type MessageReceiver interface {
+	MessageHandler() *MessageHandler
+	Setup()
+	Teardown()
+}
+
+type ReceiveEndpoint struct {
+	MessageReceiver  MessageReceiver
+	NumberOfMessages int
+	Handler          *MessageHandler
+}
+
+func NewReceiveEndpoint(receiver MessageReceiver, numberOfMessages int) *ReceiveEndpoint {
+	return &ReceiveEndpoint{
+		MessageReceiver:  receiver,
+		NumberOfMessages: numberOfMessages,
+		Handler:          receiver.MessageHandler(),
+	}
+}
+
+type MessageHandler interface {
+	// Process a received message. Return true if it's the last message, otherwise
+	// return false.
+	ReceiveMessage([]byte) bool
+
+	// Indicate whether the handler has been marked complete, meaning all messages
+	// have been received.
+	HasCompleted() bool
+}
+
+type ThroughputMessageHandler struct {
+	hasStarted       bool
+	hasCompleted     bool
+	messageCounter   int
+	NumberOfMessages int
+	started          int64
+	stopped          int64
+	completionLock   sync.Mutex
+}
+
+type LatencyMessageHandler struct {
+	NumberOfMessages int
+	Latencies        []float32
+	Results		[]byte
+	messageCounter   int
+	hasCompleted     bool
+	Channel		string
+	completionLock   sync.Mutex
+}
+
+func (handler *ThroughputMessageHandler) HasCompleted() bool {
+	handler.completionLock.Lock()
+	defer handler.completionLock.Unlock()
+	return handler.hasCompleted
+}
+
+// Increment a message counter. If this is the first message, set the started timestamp.
+// If it's the last message, set the stopped timestamp and compute the total runtime
+// and print it out. Return true if it's the last message, otherwise return false.
+func (handler *ThroughputMessageHandler) ReceiveMessage(message []byte) bool {
+	if !handler.hasStarted {
+		handler.hasStarted = true
+		handler.started = time.Now().UnixNano()
+	}
+
+	handler.messageCounter++
+
+	if handler.messageCounter == handler.NumberOfMessages {
+		handler.stopped = time.Now().UnixNano()
+		ms := float32(handler.stopped-handler.started) / 1000000.0
+		log.Printf("Received %d messages in %f ms\n", handler.NumberOfMessages, ms)
+		log.Printf("Received %f per second\n", 1000*float32(handler.NumberOfMessages)/ms)
+
+		handler.completionLock.Lock()
+		handler.hasCompleted = true
+		handler.completionLock.Unlock()
+
+		return true
+	}
+
+	return false
+}
+
+func (handler *LatencyMessageHandler) HasCompleted() bool {
+	handler.completionLock.Lock()
+	defer handler.completionLock.Unlock()
+	return handler.hasCompleted
+}
+
+// Record each message's latency. The message contains the timestamp when it was sent.
+// If it's the last message, compute the average latency and print it out. Return true
+// if the message is the last one, otherwise return false.
+func (handler *LatencyMessageHandler) ReceiveMessage(message []byte) bool {
+	now := time.Now().UnixNano()
+	then, _ := binary.Varint(message)
+	return false
+	if then != 0 {
+		handler.Latencies = append(handler.Latencies, (float32(now-then))/1000/1000)
+		if handler.Channel == "0" {
+			//log.Printf("%d \n", handler.messageCounter);
+			b:=make([]byte,8)
+			binary.PutVarint(b, (now-then))
+			x:=strconv.FormatInt(now-then, 10)
+			handler.Results=append(handler.Results, x...)
+			handler.Results=append(handler.Results, "\n"...)
+		}
+	}
+	handler.messageCounter++
+//	log.Printf(strconv.Itoa(handler.messageCounter))
+	if handler.messageCounter == handler.NumberOfMessages{
+		sum := float32(0)
+		for _, latency := range handler.Latencies {
+			sum += latency
+			
+		}
+		avgLatency := float32(sum) / float32(len(handler.Latencies))
+		//time.Sleep(5*time.Second)
+		log.Printf("Mean latency for %d messages: %f ms\n", handler.NumberOfMessages,
+			avgLatency)
+		if handler.Channel == "0" {
+			ioutil.WriteFile((handler.Channel), handler.Results, 0777)
+		}
+
+		handler.completionLock.Lock()
+		handler.hasCompleted = true
+		handler.completionLock.Unlock()
+
+		return true
+	}
+
+	return false
+}
+
+func (endpoint ReceiveEndpoint) WaitForCompletion() {
+	for {
+		if (*endpoint.Handler).HasCompleted() {
+			break
+		} else {
+			time.Sleep(1 * time.Millisecond)
+		}
+	}
+}
